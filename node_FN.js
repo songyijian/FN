2b6dbbd69df563e378fa5a2ab1e101009e2dcda2
yjsong <yj.song@sigmob.com>
2019-05-06 17:55:43 +0800

    Dates对象开始

--- node_FN.js
+++ node_FN.js
@@ -1,16 +1,26 @@
-
 /*
-node.js 简单的获取命令参数
+ * @Description: node 业务常用方法
+ * @Author: yijian.song
+ * @LastEditors: Please set LastEditors
+ * @Date: 2018-10-12 20:20:41
+ * @LastEditTime: 2019-04-22 15:49:35
+ */
 
-λ node server.js --name=ajanuw --post=14
-let a = new Argvs();
-console.log( a.argvsAll); // [ { name: 'ajanuw' }, { post: '14' } ]
-console.log( a.argvsGet('name')); // ajanuw
-console.log( a.argvsGet('post')); // 14
-console.log( a.argvsKeys()); // [ 'name', 'post' ]
-console.log( a.argvsHas('name')); // true
-*/
 
+/**
+ * @Description: 简单的获取命令参数
+ * @case: 
+ * > node server.js --name=ajanuw --post=14
+ * let a = new Argvs();
+ *     a.argvsAll           // [ { name: 'ajanuw' }, { post: '14' } ]
+ *     a.argvsGet('name')   // ajanuw
+ *     a.argvsGet('post')   // 14
+ *     a.argvsKeys()        // [ 'name', 'post' ]
+ *     a.argvsHas('name')   // true
+ * ---------
+ * @Author: yijian.song
+ * @Date: 2019-04-22 15:24:18z
+ */
 class Argvs {
   constructor() {
     this.argvsAll = this.argvsAll();
@@ -48,10 +58,15 @@ class Argvs {
 
 
 
-/*
-  获取本地
-  return {str } //127.0.0.1
-*/
+/**
+ * @Description: 获取本地ip
+ * @param {type} 
+ * @return: {str} 127.0.0.1
+ * @case: getIPAdress() >127.0.0.1
+ * ---------
+ * @Author: yijian.song
+ * @Date: 2019-04-22 15:44:24
+ */
 function getIPAdress() {
     var interfaces = require('os').networkInterfaces();
     for (var devName in interfaces) {
@@ -66,11 +81,15 @@ function getIPAdress() {
 }
 
 
-
 /**
-获取用户的ip
-@return {str } ::ffff:10.130.148.222
-*/
+ * @Description: 获取用户的ip
+ * @param {obj} req
+ * @return: {string} ::ffff:10.130.148.222
+ * @case: getClientIp(req)
+ * ---------
+ * @Author: yijian.song
+ * @Date: 2019-04-22 15:46:47
+ */
 function getClientIp(req) {
   var ip = req.headers['x-forwarded-for'] ||
     req.ip ||
--- reckonFN.js
+++ reckonFN.js
@@ -1,26 +1,14 @@
+
 // 算法
 
-
 /**
- * yijian.song
- * 2018.07.27
- * 找出简单数组中重复值的始末位置
- *  arr {array *} 数组
- *  val {string | number *} 筛选样本
- *  keep {number * } 连续重复位数 注意：别他妈给我个1，一位算重复吗？
- *
- *  return {[
- *    {
- *      val: 1,     //筛选样本
-        length: 4,  //重复了几次
-        start: 6,   //重复值在数组的始位置索引
-        end: 9      //重复值在数组的结束位置索引
-      }
- * ]}
- *
- *  RepeatBit([...'011000111101011110'],1)  >[ { val: 1, length: 4, start: 6, end: 9 },{ val: 1, length: 4, start: 13, end: 16 } ]
- * */
-
+ * 找出单层数组，重复值的始末位置
+ * @param {Array} arr 
+ * @param {string | number} val 筛选样本
+ * @param {number} keep 连续重复位数 注意：别他妈给我个1，一位算重复吗？
+ * @return {Array}  >[ { val: 1, length: 4, start: 6, end: 9 },{ val: 1, length: 4, start: 13, end: 16 } ]
+ *  RepeatBit([...'011000111101011110'],1)  
+ */
 
 function RepeatBit(arr, val, keep = 3) {
   if (!Array.isArray(arr)) { return Error('参数必须是合法数组') }
@@ -43,36 +31,3 @@ function RepeatBit(arr, val, keep = 3) {
   }
   return m
 }
-
-// 上面的更精简
-// function RepeatBit(arr,val,keep=3) {
-//   if(!Array.isArray(arr)){ return Error('参数必须是合法数组')}
-//   if (typeof val === 'undefined') { return Error('重复参考样本不能为空')}
-//   var m =[]
-//   arr.forEach((item, index) => {
-//     item == val ? m.push(index) : null
-//   })
-//   var gs = []
-//   var t=0;
-//   var y=1;
-//   for (; t < m.length-1; ){
-//     if (m[t + y] - m[t] === y) {
-//       y++
-//     }else{
-//       if(y>keep-1){
-//         gs.push({
-//           val:val,
-//           length: m[t + y - 1] - m[t]+1,
-//           start: m[t],
-//           end: m[t + y - 1]
-//         })
-//       }
-//       t=t+y;
-//       y=1;
-//     }
-//   }
-//   return gs
-// }
-
-
-
