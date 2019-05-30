6bd68a934c1453306c35680f7a2fd815890eb0e3
yjsong <yj.song@sigmob.com>
2019-05-30 14:19:27 +0800

    添加工具方法

--- JSON.js
+++ JSON.js
@@ -4,6 +4,7 @@
  *  JSON.copy()       //深拷贝
  *  JSON.delByValue   //删除key='指定值'键
  *  JSON.delKey       //删除指定key
+ *  JSON.cat          //合并对象
  * ---
  * @Author: yijian.song
  * @Date: 2019-05-06 15:06:50
@@ -12,9 +13,9 @@
 export default function () {
 
   JSON.copy = function copy(obj) {
-    let _obj = JSON.stringify(obj),
-      objClone = JSON.parse(_obj);
-    return objClone
+    let _obj = JSON.stringify(obj);
+    _obj = JSON.parse(_obj);
+    return _obj
   }
 
   /**
@@ -42,7 +43,7 @@ export default function () {
    * @param {Array|obj} json
    * @param {Array|str} rmval
    * @return: {Array|obj} 新对象
-   * @case: delByValue({a:'',b:1},[''])  //{b:1} 踢出key=‘’
+   * @case: delByValue({a:'',b:1},[''])  //{b:1}踢出key=‘’
    */
   JSON.delByVal = function delByValue(json, rmval) {
     if (typeof json !== 'object' || !json) {
@@ -62,6 +63,23 @@ export default function () {
       }
     })
     return njson
+  }
+
+  /**
+   * @Description: 合并对象，返回一个前拷贝对象
+   * @param {arr | json} [],{},...
+   * @return: {json} 浅拷贝对象
+   * @case: 
+   *  JSON.cat({A:1},{b:2},3,[1,2]) >{0: 1, 1: 2, A: 1, b: 2}
+   */
+  JSON.cat = function () {
+    var argl = arguments.length
+    var _obj = {}
+    if (argl < 1) { return }
+    for (let i = 0; i < argl; i++) {
+      _obj = Object.assign(_obj, arguments[i])
+    }
+    return _obj
   }
 
 }
