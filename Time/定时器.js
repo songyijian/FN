e63c727e5b241a95b0a36e595bc63b3249d676b0
yijian.song <yijian.song@opay-inc.com>
2022-04-12 19:20:44 +0800

    feat:tIntervarl

--- Time/定时器.js
+++ Time/定时器.js
@@ -13,18 +13,20 @@
  * @return {*}
  * @Author: yijian
  */
+
 export function tIntervarl(fn, t = 0, ...args) {
   clearTimeout(ot)
-  let time = t
-  let ot = setTimeout(function func() {
+  var time = t
+  var ot = setTimeout(function func() {
+    config.steps += 1
     fn.apply(this, args)
     ot = setTimeout(func, time)
   }, t)
-
-  let setTime = n => (time = n)
-  let clear = () => {
+  var setTime = n => (time = n)
+  var clear = () => {
     clearTimeout(ot)
     time = ot = null
   }
-  return { clear, setTime }
+  var config = { clear, setTime, steps: 0 }
+  return config
 }
